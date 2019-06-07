2a2259fbc2e4360070556de24f9189ec45c09630
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-06-07 09:02:25 -0400

    Updated Readme, added in an intro image, fixed the odd path behavior of the storyline

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -78,12 +78,13 @@ class StoryBrain {
       _storyNumber = 5;
     } else if (choiceNumber == 2 && _storyNumber == 2) {
       _storyNumber = 4;
-    }
-    //TODO: Step 22 - In nextStory() if the _storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the _storyNumber to 0.
-    if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      //TODO: Step 22 - In nextStory() if the _storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the _storyNumber to 0.
       print(_storyNumber);
       reset();
     }
+    
+   
   }
 
   void reset() {
